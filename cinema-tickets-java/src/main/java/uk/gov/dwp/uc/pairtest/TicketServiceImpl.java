c573392338012f18bf68290ec94b5c1f12c153b7
cadburl312@gmail.com <cadburyl312@gmail.com>
2026-05-01 10:46:12 +0100

    Completed the coding assessment will all tests added

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -1,16 +1,72 @@
 package uk.gov.dwp.uc.pairtest;
 
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+
+import java.util.*;
+
+import static uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type.*;
 
 public class TicketServiceImpl implements TicketService {
     /**
      * Should only have private methods other than the one below.
      */
 
-    @Override
-    public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+    private static final int MAX_TICKETS = 25;
+    private static final int ADULT_PRICE = 25;
+    private static final int CHILD_PRICE = 15;
+    private final TicketPaymentService ticketPaymentService;
+    private final SeatReservationService seatReservationService;
 
+    public TicketServiceImpl(TicketPaymentService ticketPaymentService, SeatReservationService seatReservationService) {
+        this.ticketPaymentService = ticketPaymentService;
+        this.seatReservationService = seatReservationService;
     }
 
+    @Override
+    public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
+        if (ticketTypeRequests == null || ticketTypeRequests.length == 0) {
+            throw new InvalidPurchaseException();
+        }
+
+        if (accountId == null || accountId <= 0) {
+            throw new InvalidPurchaseException();
+        }
+
+        int sumAdults = 0;
+        int sumInfants = 0;
+        int totalPrice = 0;
+        int numSeats = 0;
+
+        for (TicketTypeRequest ticketTypeRequest : ticketTypeRequests) {
+            if (ticketTypeRequest == null || ticketTypeRequest.getNoOfTickets() <= 0) {
+                throw new InvalidPurchaseException();
+            }
+
+            if (ticketTypeRequest.getTicketType() == ADULT) {
+                totalPrice += ADULT_PRICE * ticketTypeRequest.getNoOfTickets();
+                numSeats += ticketTypeRequest.getNoOfTickets();
+                sumAdults += ticketTypeRequest.getNoOfTickets();
+            }
+            else if (ticketTypeRequest.getTicketType() == CHILD) {
+                totalPrice += CHILD_PRICE * ticketTypeRequest.getNoOfTickets();
+                numSeats += ticketTypeRequest.getNoOfTickets();
+            }
+            else {
+                sumInfants += ticketTypeRequest.getNoOfTickets();
+            }
+        }
+
+        if (sumAdults == 0 || sumInfants > sumAdults) {
+            throw new InvalidPurchaseException();
+        }
+        if (numSeats > MAX_TICKETS) {
+            throw new InvalidPurchaseException();
+        }
+
+        ticketPaymentService.makePayment(accountId, totalPrice);
+        seatReservationService.reserveSeat(accountId, numSeats);
+    }
 }
---
+++ cinema-tickets-java/src/test/java/TicketServiceTest.java
@@ -0,0 +1,235 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import thirdparty.paymentgateway.TicketPaymentService;
+import thirdparty.seatbooking.SeatReservationService;
+import uk.gov.dwp.uc.pairtest.TicketService;
+import uk.gov.dwp.uc.pairtest.TicketServiceImpl;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.mockito.Mockito.*;
+import static uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest.Type.*;
+
+public class TicketServiceTest {
+    TicketPaymentService paymentServiceMock;
+    SeatReservationService seatReservationServiceMock;
+    TicketService ticketService;
+
+    @BeforeEach
+    public void setup() {
+        paymentServiceMock = mock(TicketPaymentService.class);
+        seatReservationServiceMock = mock(SeatReservationService.class);
+        ticketService = new TicketServiceImpl(paymentServiceMock, seatReservationServiceMock);
+    }
+
+    private static Stream<Long> invalidAccountIds() {
+        return Stream.of(null, 0L, -1L);
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidAccountIds")
+    public void purchaseTickets_whenGivenInvalidAccountId_throwsInvalidPurchaseException(Long accountId) {
+        assertThrows(InvalidPurchaseException.class,
+                () -> ticketService.purchaseTickets(accountId, new TicketTypeRequest(ADULT, 15))
+        );
+        verifyNoInteractions(paymentServiceMock, seatReservationServiceMock);
+    }
+
+    @Test
+    public void purchaseTickets_whenGivenNullTicketArray_throwsInvalidPurchaseException() {
+        assertThrows(InvalidPurchaseException.class, () ->
+            ticketService.purchaseTickets(1L, (TicketTypeRequest[]) null)
+        );
+        verifyNoInteractions(paymentServiceMock, seatReservationServiceMock);
+    }
+
+    @Test
+    public void purchaseTickets_whenGivenEmptyTicketArray_throwsInvalidPurchaseException() {
+        assertThrows(InvalidPurchaseException.class, () ->
+                ticketService.purchaseTickets(1L)
+        );
+        verifyNoInteractions(paymentServiceMock, seatReservationServiceMock);
+    }
+
+    private static Stream<Arguments> invalidTicketArrayContainingNull() {
+        return Stream.of(
+                arguments(1L, new TicketTypeRequest[]{
+                        null,
+                        new TicketTypeRequest(ADULT, 14)
+                }),
+                arguments(2L, new TicketTypeRequest[]{
+                        new TicketTypeRequest(ADULT, 16),
+                        null
+                }),
+                arguments(2L, new TicketTypeRequest[]{
+                        null
+                })
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidTicketArrayContainingNull")
+    public void purchaseTickets_whenGivenTicketArrayContainingNull_throwsInvalidPurchaseException(long accountId, TicketTypeRequest[] requests) {
+        assertThrows(InvalidPurchaseException.class, () ->
+            ticketService.purchaseTickets(accountId, requests)
+        );
+        verifyNoInteractions(paymentServiceMock, seatReservationServiceMock);
+    }
+
+    private static Stream<Arguments> invalidMoreThanMaxTicketsAreOrdered() {
+        return Stream.of(
+                arguments(1L, new TicketTypeRequest[]{
+                        new TicketTypeRequest(ADULT, 15),
+                        new TicketTypeRequest(CHILD, 14)
+                }),
+                arguments(2L, new TicketTypeRequest[]{
+                        new TicketTypeRequest(ADULT, 26)
+                })
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidMoreThanMaxTicketsAreOrdered")
+    public void purchaseTickets_WhenMoreThanMaxTicketsAreOrdered_throwsInvalidPurchaseException(long accountId, TicketTypeRequest[] requests) {
+
+        assertThrows(InvalidPurchaseException.class, () ->
+            ticketService.purchaseTickets(accountId,requests)
+        );
+        verifyNoInteractions(paymentServiceMock, seatReservationServiceMock);
+    }
+
+    private static Stream<Arguments> invalidNoAdultTicketsAreOrdered() {
+        return Stream.of(
+                arguments(1L, new TicketTypeRequest[]{
+                        new TicketTypeRequest(CHILD, 1)
+                }),
+                arguments(2L, new TicketTypeRequest[]{
+                        new TicketTypeRequest(INFANT, 1),
+                        new TicketTypeRequest(CHILD, 1)
+                }),
+                arguments(3L, new TicketTypeRequest[]{
+                        new TicketTypeRequest(INFANT, 2),
+                })
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidNoAdultTicketsAreOrdered")
+    public void purchaseTickets_whenNoAdultsTicketsAreOrdered_throwsInvalidPurchaseException(long accountId, TicketTypeRequest[] requests) {
+        assertThrows(InvalidPurchaseException.class, () ->
+            ticketService.purchaseTickets(accountId,requests)
+        );
+        verifyNoInteractions(paymentServiceMock, seatReservationServiceMock);
+    }
+
+    private static Stream<Arguments> invalidMoreInfantThanAdultTicketsAreOrdered() {
+        return Stream.of(
+                arguments(1L, new TicketTypeRequest[]{
+                        new TicketTypeRequest(INFANT, 1)
+                }),
+                arguments(2L, new TicketTypeRequest[]{
+                        new TicketTypeRequest(INFANT, 2),
+                        new TicketTypeRequest(ADULT, 1)
+                }),
+                arguments(3L, new TicketTypeRequest[]{
+                        new TicketTypeRequest(INFANT, 2),
+                        new TicketTypeRequest(CHILD, 1),
+                        new TicketTypeRequest(ADULT, 1)
+                })
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidMoreInfantThanAdultTicketsAreOrdered")
+    public void purchaseTickets_whenMoreInfantTicketsThanAdultTicketsAreOrdered_throwsInvalidPurchaseException(long accountId, TicketTypeRequest[] requests) {
+        assertThrows(InvalidPurchaseException.class, () ->
+                ticketService.purchaseTickets(accountId,requests)
+        );
+        verifyNoInteractions(paymentServiceMock, seatReservationServiceMock);
+    }
+
+    private static Stream<Arguments> invalidZeroOrLessNumberOfTicketsAreOrderedInAnyTicketRequest() {
+        return Stream.of(
+                arguments(1L, new TicketTypeRequest[]{
+                        new TicketTypeRequest(ADULT, 1),
+                        new TicketTypeRequest(ADULT, -2)
+                }),
+                arguments(1L, new TicketTypeRequest[]{
+                        new TicketTypeRequest(ADULT, 0),
+                        new TicketTypeRequest(ADULT, 4)
+                }),
+                arguments(1L, new TicketTypeRequest[]{
+                        new TicketTypeRequest(CHILD, 0),
+                        new TicketTypeRequest(ADULT, 4)
+                }),
+                arguments(1L, new TicketTypeRequest[]{
+                        new TicketTypeRequest(CHILD, -1),
+                        new TicketTypeRequest(ADULT, 4)
+                }),
+                arguments(1L, new TicketTypeRequest[]{
+                        new TicketTypeRequest(INFANT, 0),
+                        new TicketTypeRequest(ADULT, 4)
+                }),
+                arguments(1L, new TicketTypeRequest[]{
+                        new TicketTypeRequest(INFANT, -1),
+                        new TicketTypeRequest(ADULT, 4)
+                })
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("invalidZeroOrLessNumberOfTicketsAreOrderedInAnyTicketRequest")
+    public void purchaseTickets_whenGivenTicketWithZeroOrLessNumberOfTicketsToOrder_throwsInvalidPurchaseException(long accountId, TicketTypeRequest[] requests) {
+        assertThrows(InvalidPurchaseException.class, () ->
+            ticketService.purchaseTickets(accountId,requests)
+        );
+        verifyNoInteractions(paymentServiceMock, seatReservationServiceMock);
+    }
+
+    private static Stream<Arguments> validTickets() {
+        return Stream.of(
+                arguments(1L, 375, 15, new TicketTypeRequest[]{
+                        new TicketTypeRequest(ADULT, 15),
+                }),
+                arguments(1L, 625, 25, new TicketTypeRequest[]{
+                        new TicketTypeRequest(ADULT, 25),
+                }),
+                arguments(1L, 625, 25, new TicketTypeRequest[]{
+                        new TicketTypeRequest(ADULT, 25),
+                        new TicketTypeRequest(INFANT, 10),
+                }),
+                arguments(1L, 325, 15, new TicketTypeRequest[]{
+                        new TicketTypeRequest(ADULT, 10),
+                        new TicketTypeRequest(CHILD, 5),
+                }),
+                arguments(1L, 250, 10, new TicketTypeRequest[]{
+                        new TicketTypeRequest(ADULT, 10),
+                        new TicketTypeRequest(INFANT, 10),
+                }),
+                arguments(1L, 250, 10, new TicketTypeRequest[]{
+                        new TicketTypeRequest(ADULT, 5),
+                        new TicketTypeRequest(ADULT, 5),
+                        new TicketTypeRequest(INFANT, 10),
+                }),
+                arguments(1L, 400, 20, new TicketTypeRequest[]{
+                        new TicketTypeRequest(ADULT, 10),
+                        new TicketTypeRequest(CHILD, 10),
+                        new TicketTypeRequest(INFANT, 10),
+                })
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("validTickets")
+    public void purchaseTickets_whenGivenValidTicket_CorrectlyCallsPaymentServiceAndSeatReservationService(long accountId, int expectedPrice, int expectedSeat, TicketTypeRequest[] requests) {
+        ticketService.purchaseTickets(accountId,requests);
+        verify(paymentServiceMock).makePayment(accountId, expectedPrice);
+        verify(seatReservationServiceMock).reserveSeat(accountId, expectedSeat);
+        verifyNoMoreInteractions(paymentServiceMock, seatReservationServiceMock);
+    }
+}
